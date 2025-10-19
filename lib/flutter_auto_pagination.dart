4bf3e9b4fccc23cf484dfb4a8f1e268a339aa345
Amin Zamani Afshar <zamani35099@gmail.com>
2025-10-19 20:00:03 +0330

    refactor: improve names

--- lib/flutter_auto_pagination.dart
+++ lib/flutter_auto_pagination.dart
@@ -1,5 +1,4 @@
-export 'src/pagination_controller.dart';
+export 'src/auto_pagination_mixin.dart';
 export 'src/pagination_state.dart';
 export 'src/pagination_data.dart';
-export 'src/listenable_pagination_controller.dart';
-export 'src/pagination_view.dart';
+export 'src/auto_pagination.dart';
